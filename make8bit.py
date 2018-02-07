23e23400b1d0256891c1a6d9b4324d8457e13bde
Velizar Efremov <velizar.efremov@yandex.ru>
2018-02-07 19:43:39 +0100

    small functions

---
+++ make8bit.py
@@ -0,0 +1,13 @@
+from itkutilities import get_itk_array, write_itk_imageArray
+import numpy as np
+import sys
+
+if __name__ == '__main__':
+
+    # Read file
+    inputimg = np.array(get_itk_array(sys.argv[1]))
+
+    inputimg = (inputimg/256).astype('uint8')
+
+    # Write to File
+    write_itk_imageArray(inputimg, sys.argv[2])
